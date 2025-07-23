e02167651d85931070591a1c1e4de98f548f493a
amsilia <kozamaria78@gmail.com>
2025-07-23 21:11:10 +0800

    Remove incorrect BE/scripts folder

--- scripts/BE/scripts/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "=== STARTING BACKEND ==="
-
-cd /home/ubuntu/app/BE || exit
-pm2 start server.js --name backend-app || npm run start
--- scripts/BE/scripts/stop.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "=== STOPPING BACKEND ==="
-
-pm2 stop backend-app || echo "PM2 process not found. Skipping stop."
-
